59cec0918886871015fca16e60569fda2d65bb9b
Ming <oncwnuAUeYAFR4UGlq8BEJq8Jy-k@git.weixin.qq.com>
2026-09-07 08:53:40 +0800

    chore: point website repository to ScriptPace organization

--- site-config.js
+++ site-config.js
@@ -1,6 +1,6 @@
 window.SCRIPTPACE_SITE = Object.freeze({
   appStoreURL: "https://apps.apple.com/app/id6805241939",
   supportEmail: "breet.jia@gmail.com",
-  repositoryURL: "https://github.com/hddevteam/ScriptPaceWebsite",
+  repositoryURL: "https://github.com/scriptpace/scriptpace.github.io",
   version: "1.0"
 });
